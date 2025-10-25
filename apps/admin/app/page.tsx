891f5ab8615ff3d324ef47e7950e950d2cbd77a8
JAYAKUMAR\jayak <jayakumar.developer@gmail.com>
2025-10-25 19:10:18 +0530

    1

--- apps/admin/app/page.tsx
+++ apps/admin/app/page.tsx
@@ -1,7 +1,7 @@
 import Image from 'next/image';
 
 export default function Home() {
-  const a = 20;
+  const e = 20;
   return (
     <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
       <main className="flex min-h-screen w-full max-w-3xl flex-col items-center justify-between bg-white px-16 py-32 sm:items-start dark:bg-black">
